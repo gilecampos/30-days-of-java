cbaa197ce783639f784f8b2740d2e4f96743b094
gilecampos <gileandrocampos8@gmail.com>
2025-12-15 17:45:10 -0300

    feat: add content for VariablesDemo

--- src/day01_basics/VariablesDemo.java
+++ src/day01_basics/VariablesDemo.java
@@ -2,6 +2,16 @@ package day01_basics;
 
 public class VariablesDemo {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        int idade = 25;
+        double salarioMensal = 4500.75;
+        boolean isBackendDeveloper = true;
+
+        // Convenção camelCase
+        String nomeCompleto = "Gileandro Campos";
+
+        System.out.println("Nome: " + nomeCompleto);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salarioMensal);
+        System.out.println("Backend Dev? " + isBackendDeveloper);
     }
 }
