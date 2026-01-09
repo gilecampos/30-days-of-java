159676032d5b1aa827f6f2b5cccbe1c294908eb5
gilecampos <gileandrocampos8@gmail.com>
2026-01-09 16:27:37 -0300

    add: Review for arithmetic operators

---
+++ src/day02_operators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package day02_operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a + b); // Soma
+        System.out.println(a - b); // Subtração
+        System.out.println(a * b); // Multiplicação
+        System.out.println(a / b); // Divisão inteira
+        System.out.println( a % b); // resto
+    }
+}
