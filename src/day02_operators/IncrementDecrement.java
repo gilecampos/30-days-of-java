96b929a46dee47bf79fc84cd8c52d3e8a1eb4f15
gilecampos <gileandrocampos8@gmail.com>
2026-01-09 16:48:19 -0300

    add: Review for Increment and Decrement operators

---
+++ src/day02_operators/IncrementDecrement.java
@@ -0,0 +1,13 @@
+package day02_operators;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 5;
+
+        System.out.println(x++);
+        System.out.println(++x);
+
+        System.out.println(x--);
+        System.out.println(--x);
+    }
+}
