30cfa480bc36643b7097de23d790da51527361f3
gilecampos <gileandrocampos8@gmail.com>
2026-01-09 16:46:49 -0300

    add: Review for assignment  operators

---
+++ src/day02_operators/AssignmentOperators.java
@@ -0,0 +1,12 @@
+package day02_operators;
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int value = 10;
+
+        value += 5;
+        value -= 5;
+        value *= 5;
+        value /= 5;
+    }
+}
