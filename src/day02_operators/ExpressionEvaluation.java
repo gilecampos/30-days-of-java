b5c1bae6c3e3093994eb343776e3ffb34e7279e7
gilecampos <gileandrocampos8@gmail.com>
2026-01-09 16:54:06 -0300

    add: Review for Expression Evaluation

---
+++ src/day02_operators/ExpressionEvaluation.java
@@ -0,0 +1,11 @@
+package day02_operators;
+
+public class ExpressionEvaluation {
+    public static void main(String[] args) {
+        int result = 10 + 5 * 2;
+        System.out.println(result);
+
+        int result2 = (10 + 5) * 2;
+        System.out.println(result2);
+    }
+}
