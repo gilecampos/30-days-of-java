d0f51224df355c6526886cea6995c01f51bdddcd
gilecampos <gileandrocampos8@gmail.com>
2026-01-09 16:37:25 -0300

    add: Review for relational operators

---
+++ src/day02_operators/RelationalOperators.java
@@ -0,0 +1,14 @@
+package day02_operators;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 15;
+
+        System.out.println(x > y);
+        System.out.println(x >= y);
+        System.out.println(x < y);
+        System.out.println(x == y);
+        System.out.println(x != y);
+    }
+}
